521b0b200f13558ef49363dbd61c23d06b440515
Breyanna Vass <breyannavass@gmail.com>
2019-09-30 21:12:45 -0400

    understanding variable types

--- main.js
+++ main.js
@@ -2,7 +2,21 @@
 exports.__esModule = true;
 var message = "Welcome back";
 console.log(message);
+// variable declarations
 var x = 10;
 var y = 20;
 var sum;
 var title = "Codevolution";
+//variable types
+var isBeginner = true;
+var total = 0;
+var name = "Mike";
+var sentence = "My name is " + name + "\nI am a beginner in Typescript";
+console.log(sentence);
+var n = null;
+var u = undefined;
+var isNew = null;
+var myName = undefined;
+// interchangable syntax for arrays
+var list1 = [1, 2, 3];
+var list2 = [1, 2, 3];
--- main.ts
+++ main.ts
@@ -2,8 +2,29 @@ export {};
 let message = "Welcome back";
 console.log(message);
 
+// variable declarations
 let x = 10;
 const y = 20;
 
 let sum;
 const title = "Codevolution";
+
+//variable types
+let isBeginner: boolean = true;
+let total: number = 0;
+let name: string = "Mike";
+
+let sentence: string = `My name is ${name}
+I am a beginner in Typescript`;
+
+console.log(sentence);
+
+let n: null = null;
+let u: undefined = undefined;
+
+let isNew: boolean = null;
+let myName: string = undefined;
+
+// interchangable syntax for arrays
+let list1: number[] = [1, 2, 3];
+let list2: Array<number> = [1, 2, 3];
