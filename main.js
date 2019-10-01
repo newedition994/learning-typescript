0b68fa26ceab5bbbacea4f24fb85fb907b3766b7
Breyanna Vass <breyannavass@gmail.com>
2019-09-30 21:04:25 -0400

    learning variable declarations

--- main.js
+++ main.js
@@ -2,3 +2,7 @@
 exports.__esModule = true;
 var message = "Welcome back";
 console.log(message);
+var x = 10;
+var y = 20;
+var sum;
+var title = "Codevolution";
--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
 export {};
 let message = "Welcome back";
 console.log(message);
+
+let x = 10;
+const y = 20;
+
+let sum;
+const title = "Codevolution";
